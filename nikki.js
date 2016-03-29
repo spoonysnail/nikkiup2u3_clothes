f8f6823ba012256262402e16b824934c2cff84b4
spoonysnail <spoonysnailgjl@gmail.com>
2016-03-29 12:32:19 +0800

    Update nikki.js

--- nikki.js
+++ nikki.js
@@ -111,7 +111,7 @@ function clickableTd(piece) {
   return "<td id='clickable-" + (type + id) + "' class='" + cls
       + "'><a href='#dummy' class='button' " + tooltip
       + "onClick='toggleInventory(\"" + type + "\",\"" + id + "\")'>"
-      + name + "&nbps;"+calRel(type+id) + "</a></td>";
+      + name + "&nbsp;"+calRel(type+id) + "</a></td>";
 }
 
 function row(piece, isShoppingCart) {
